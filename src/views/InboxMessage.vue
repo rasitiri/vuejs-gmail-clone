c355b5521f8f1dba2d1c0ec48f84fec27831cbdf
rasitiri <rasitiri3@gmail.com>
2020-08-09 18:46:02 +0300

    refactoring for responsive

--- src/views/InboxMessage.vue
+++ src/views/InboxMessage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overflow-y-auto height-full">
+  <div class="overflow-y-auto height-full lg:h-auto">
     <mail-list v-for="i in 20" :key="i"></mail-list>
   </div>
 </template>
@@ -15,5 +15,10 @@ export default {
 <style scoped>
 .height-full {
   height: 35.7rem;
+}
+@media (max-width: 700px) {
+  .height-full {
+    height: 28.4rem;
+  }
 }
 </style>
